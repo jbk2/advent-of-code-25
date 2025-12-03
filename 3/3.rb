8de99c8fd080267591751852ab343ff547b3b66b
jbk2 <james@humanbenefits.com>
2025-12-03 16:08:39 +0100

    Iterative solution for 3.1

---
+++ 3/3.rb
@@ -0,0 +1,54 @@
+require_relative "../utils"
+require 'benchmark'
+
+# each line = a bank, each number = a battery's joltage
+# can take two batteries from a bank
+# find max joltage per bank
+# total output = sum of max joltage from each bank
+
+def max_joltage(input)
+  bank_maximums = []
+
+  input.each do |bank|
+    bank_arr = bank.chars
+    max_val = 0
+
+    bank_arr.each_with_index do |first_battery, index|
+      bank_remainder = bank_arr[index+1..-1]
+      
+      bank_remainder.each do |second_battery|
+        battery_pair_joltage = (first_battery + second_battery).to_i
+        max_val = [battery_pair_joltage, max_val].max
+        # binding.irb
+      end
+    end
+
+    bank_maximums << max_val
+  end
+  bank_maximums.reduce(:+)
+end
+
+
+
+####################################
+TEST_DATA = ["987654321111111","811111111111119","234234234234278","818181911112111"]
+REAL_DATA = fetch_puzzle_input(3)
+
+puts "Running test 1"
+result = max_joltage(TEST_DATA)
+puts result === 357 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
+
+puts "Running real data test 1"
+result = max_joltage(REAL_DATA)
+puts result === 17427 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
+
+# puts "Running test 2"
+# result = invalid_ids_sum_three(TEST_DATA)
+# puts result === 4174379265 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
+
+# puts "Running real data test 2"
+# time = Benchmark.measure do
+#   result = invalid_ids_sum_three(REAL_DATA)
+# end
+# puts 'time taken -> ', time
+# puts result === 30962646823 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
