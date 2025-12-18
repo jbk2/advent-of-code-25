d5ba194e3e3070861cc58f11515eba5b9a27ca28
jbk2 <james@humanbenefits.com>
2025-12-18 15:37:44 +0100

    day 10 part 1 - parse data input

--- day10/10.rb
+++ day10/10.rb
@@ -1,30 +1,38 @@
 require_relative "../utils"
 require 'benchmark'
+def split_data(input)
+  input.map do |str|
+    arr = str.split(' ')
+    lights, *buttons, batteries = arr
+    return { lights: lights, buttons: buttons, batteries: batteries  } 
+  end
+end
+
 
 ####################################
-REAL_DATA = fetch_puzzle_input(9)
+REAL_DATA = fetch_puzzle_input(10)
 # col, row - 0 indexed
-TEST_DATA = ["7,1", "11,1", "11,7", "9,7", "9,5", "2,5", "2,3", "7,3"]
+TEST_DATA = ['[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}']
 # puts sort_by_row(TEST_DATA).each_cons(1) { |e| area(e[0], e[1]) }.inspect 
 # puts largest_rect(TEST_DATA)
 # puts largest_rect(TEST_DATA).each {|e| puts e.inspect }
 # puts REAL_DATA.inspect
+puts split_data(TEST_DATA).inspect
+# puts "Running test 1"
+# result = largest_rect(TEST_DATA)
+# puts result === 50 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
 
-puts "Running test 1"
-result = largest_rect(TEST_DATA)
-puts result === 50 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
+# puts "Running real data test 1"
+# result = largest_rect(REAL_DATA)
+# puts result === 4748769124 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
 
-puts "Running real data test 1"
-result = largest_rect(REAL_DATA)
-puts result === 4748769124 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
+# puts "Running test 2"
+# result = largest_filled_rect(TEST_DATA)
+# puts result === 24 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
 
-puts "Running test 2"
-result = largest_filled_rect(TEST_DATA)
-puts result === 24 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
-
-puts "Running real data test 2"
-time = Benchmark.measure do
-  result = largest_filled_rect(REAL_DATA)
-end
-puts 'time taken -> ', time
-puts result === 1525991432 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
+# puts "Running real data test 2"
+# time = Benchmark.measure do
+#   result = largest_filled_rect(REAL_DATA)
+# end
+# puts 'time taken -> ', time
+# puts result === 1525991432 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
