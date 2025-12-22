dcfbe97d467b476ba88201c2f381b86b5150afdc
jbk2 <james@humanbenefits.com>
2025-12-22 17:28:56 +0000

    day 10 part 1 - turn lights into bits & bitmask the buttons

--- day10/10.rb
+++ day10/10.rb
@@ -5,14 +5,31 @@ def split_data(input)
   input.map do |str|
     arr = str.split(' ')
     lights, *buttons, batteries = arr
-    data = {
-      lights: lights.gsub!(/[\[\]]/, ""),
-      buttons: buttons.map { |button| button.delete!("()") },
-      batteries: batteries.delete!("{}")
-    }
-    return data
+    lights_count = lights.length
+    
+    lights.gsub!(/[\[\]]/, "").split('').map { |el| el == '#' ? 1 : 0 }
+    
+    buttons.map! do |button|        
+      btn_arr = button.delete!("()").split(',').map(&:to_i)
+      # binding.irb
+      btn_bitmask(lights_count, btn_arr)
+    end
+  
+    batteries.delete!("{}").split(',').map(&:to_i)
+  
+    return { lights: lights, buttons: buttons, batteries: batteries }
   end
 end
+
+def btn_bitmask(length, btn_arr)
+  mask = Array.new(length, 0)
+  btn_arr.each { |i| mask[i] = 1 }
+  return mask
+end
+
+
+
+
 
 
 ####################################
@@ -23,8 +40,8 @@ TEST_DATA = ['[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}']
 # puts largest_rect(TEST_DATA)
 # puts largest_rect(TEST_DATA).each {|e| puts e.inspect }
 # puts REAL_DATA.inspect
-split_data(TEST_DATA) => { lights:, buttons:, batteries: }
-puts lights.inspect
+# split_data(TEST_DATA) => { lights:, buttons:, batteries: }
+puts split_data(TEST_DATA).inspect
 # puts "Running test 1"
 # result = largest_rect(TEST_DATA)
 # puts result === 50 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
