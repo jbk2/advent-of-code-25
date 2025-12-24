27bc00e00d69c0cd8fb3dab305034d1e7caaa85a
jbk2 <james@humanbenefits.com>
2025-12-24 19:16:25 +0000

    Day10 pt.2 - building out helper methods

--- day10/10.rb
+++ day10/10.rb
@@ -16,7 +16,7 @@ def split_data(input)
       btn_bitmask(lights_count, btn_arr)
     end
   
-    batteries.delete!("{}").split(',').map(&:to_i)
+    batteries = batteries.delete!("{}").split(',').map(&:to_i)
   
     { lights: lights, buttons: buttons, batteries: batteries }
   end
@@ -31,7 +31,6 @@ end
 def total_combos(input)
   data = split_data(input)
   total_btn_presses = []
-  successful_combos = []
   
   data.each do |machine|
     lights, buttons = machine.values_at(:lights, :buttons)
@@ -57,15 +56,66 @@ def total_combos(input)
       end
       break if found
     end
-    # successful_combos
   end
   total_btn_presses.sum
 end
 
+def counts_to_joltage(input)
+  data = split_data(input)
+  total_btn_presses = 0
+
+  data.each do |machine|
+    buttons, batteries = machine.values_at(:buttons, :batteries)
+    remaining_joltage = batteries.dup
+    btn_count = buttons.length
+
+    
+    (1..btn_count).times do |i|
+      buttons.combination(i) do |combinations|
+        
+      end
+    end
+
+  end
+  # 
+  #
+  #
+end
+
+def remove_unviable_batteries(buttons, remaining)
+  remaining.each_with_index do |joltage, idx|
+    if joltage < 1
+      buttons.reject! { |bat| bat[idx] == 1 }
+    end
+  end
+  buttons
+end
+
+def press_btn_times(remaining, button, times)
+  return remaining if times < 1
+  updated_remaining = remaining.dup
+
+  button.each_with_index do |battery, idx|
+    next unless battery == 1
+    if updated_remaining[idx] > 0
+      updated_remaining[idx] -= 1
+    else
+      return false
+    end
+  end
+
+  updated_remaining
+end
+
+def min_remainign_presses(remaining)
+  remaining.max
+end
+
 ####################################
 REAL_DATA = fetch_puzzle_input(10)
-# col, row - 0 indexed
 TEST_DATA = ['[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}', '[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}', '[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}']
+
+puts counts_to_joltage(TEST_DATA)
 
 puts "Running test 1"
 result = total_combos(TEST_DATA)
@@ -76,8 +126,8 @@ result = total_combos(REAL_DATA)
 puts result === 507 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
 
 # puts "Running test 2"
-# result = largest_filled_rect(TEST_DATA)
-# puts result === 24 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
+# result = countrs_to_joltage(TEST_DATA)
+# puts result === 33 ? colorize("test Passed", 32) : colorize("test failed with result of #{result}", 31)
 
 # puts "Running real data test 2"
 # time = Benchmark.measure do
